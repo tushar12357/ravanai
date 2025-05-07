9b66dcbe3f5f7673cdd95c158241f6419536a872
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-05-07 16:18:25 +0530

    widget context

--- src/components/Ravan.tsx
+++ src/components/Ravan.tsx
@@ -17,7 +17,7 @@ const RavanVoiceAI = () => {
   const [isMuted, setIsMuted] = useState(false);
   const [speech, setSpeech] = useState("");
   const [isVisible, setIsVisible] = useState(true);
-  // const {agent_id,schema}=useWidgetContext()
+  const {agent_id,schema}=useWidgetContext()
   const [auto_end_call, setAutoEndCall] = useState(false);
   const [pulseEffects, setPulseEffects] = useState({
     small: false,
@@ -40,8 +40,8 @@ const RavanVoiceAI = () => {
     setStatus,
   } = useUltravoxStore();
   const baseurl = "https://app.snowie.ai";
-  const agent_id = "43279ed4-9039-49c8-b11b-e90f3f7c588c";
-  const schema = "6af30ad4-a50c-4acc-8996-d5f562b6987f";
+  // const agent_id = "43279ed4-9039-49c8-b11b-e90f3f7c588c";
+  // const schema = "6af30ad4-a50c-4acc-8996-d5f562b6987f";
   const debugMessages = new Set(["debug"]);
   const orange = "#F97316";
   const creamYellow = "#FFF7ED";
