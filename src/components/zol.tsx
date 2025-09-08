c6cae02c892bfc48e227f3ac13f2bb6907b8263a
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-09-09 01:33:03 +0530

    white

--- src/components/zol.tsx
+++ src/components/zol.tsx
@@ -480,7 +480,7 @@ const Zol = () => {
               />
             </div>
           </button>
-          <span className="talk-to-me text-sm font-medium px-3 py-1 mt-2">
+          <span className="talk-to-me !text-white text-sm font-medium px-3 py-1 mt-2">
             Talk to Francisco
           </span>{" "}
         </div>
